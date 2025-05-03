21c0cd0a6f0db0bd2f893481c02a7325ad1802da
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-03 22:25:18 +0530

    database connect successfully

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(DATABASE_URL);
+        console.log("Database connection successfully");
+    }catch(error){
+        console.log("Failed to connect with Database")
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
