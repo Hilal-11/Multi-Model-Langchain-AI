069f06b68789dada7ecdb65f8fe078a6c1e3f006
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-05-05 03:07:10 +0530

    controllers

--- Backend/routes/app_routes.js
+++ Backend/routes/app_routes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 
 const signUp = require('../controllers/');
 const login = require('../controllers/');
+const logout = require('../controllers/');
+const userDetails = require('../controllers/')
 
 
-router.post();
-router.post()
+router.post('/signUp', signUp);
+router.post('/login' , login);
+router.post('/logout' , logout);
+router.post('/getUserInfo' , userDetails);
 
 
 module.exports = router;
